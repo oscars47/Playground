32718e5f93f76b59d47ce1351bc0e6c6130622cf
oscars47 <scholinoscar@gmail.com>
2023-01-08 00:50:22 -0800

    added yearly

---
+++ 2023/yearly.py
@@ -0,0 +1,7 @@
+###
+# file to solve MIT's "yearly" challenge.
+# @ oscars47
+# problem description: how many integers from 1 to 100 can you form using the digits of the year (e.g., 2023)
+#                       exactly once each along eith the operators + - * / and ^ (exponentiate).
+#                       preference for fewer operations; with the same number of operations prefer the order of the year.
+###
