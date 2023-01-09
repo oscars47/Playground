f86354823923606adecfcdc61700974a40e0d254
oscars47 <scholinoscar@gmail.com>
2023-01-08 18:56:12 -0800

    correct temp_all

--- 2023/yearly.py
+++ 2023/yearly.py
@@ -59,42 +59,87 @@ int_value = []
 #print(p_perm[0])
 
 # custom function to calculate operation perms
-def get_op_perms(p_ls, input): # takes in current p list and input numbers
+def get_op_perms(p_ls, input=0): # takes in current p list and input numbers
     op_temp_all_f = [] # list to hold formatted op_temps
 
     def temp_format(op_temp): # nested function to format the op_temps from [(2), (1, 2, 3, 4, 5), (0)] => [(2, 1, 0), (2, 2, 0), etc]
-        for op_tuple in op_temp:
-            temp = []
-            for l in operations:
-                if len(op_tuple) ==1: # if just a single number
-                    temp.append(op_tuple[0])
-                else:
-                    temp.append(op_tuple[l])
-        return temp
+        temp_all = []
+        # get count of full operations lists; this determines the r value for permutations
+        r = op_temp.count(operations)
+        if r > 0:
+            # add restrictive permutations as well
+            choice_perm = []
+            for i in range(len(operations)):
+                restricted_op = operations[:len(operations)-i]
+                # print(restricted_op)
+                # duplicate each element 4 times
+                raw_perm = []
+                for num in restricted_op:
+                    for m in range(4):
+                        raw_perm.append(num)
+                #print(raw_perm)
+                # now get permutations
+                processed_perm = list(permutations(raw_perm, r))
+                # get unique processed perms
+                processed_perm = list(set(processed_perm))
+                #print(processed_perm)
+                choice_perm.append(processed_perm)
+            
+            #combine them all
+            choice_perm_all = []
+            for choice_ls in choice_perm:
+                for choice in choice_ls:
+                    choice_perm_all.append(choice)
+            # get unique
+            choice_perm_all = list(set(choice_perm_all))
+            # print(choice_perm_all)
 
-    def is_leading_0(op_temp): # call function to check if leading 0s; if true, then call int_split
-
-    def split_int()
-
-    def compute_val()
 
 
+            # print('op_temp', op_temp)
+            #print('choice perm', choice_perm)
+            
+            # iterate through all of the op_temps; if full operations, assign each of the r-permutations
+            for choice in choice_perm_all: # do this r times
+                temp = []
+                c = 0 # counter to tell how many of the r we've done, so what integer to choose in perm tuple
+                for op_tuple in op_temp:
+                    if len(op_tuple) ==1: # if just a single number
+                        temp.append(op_tuple[0])
+                    else:
+                        temp.append(choice[c])
+                        c+=1
+                temp_all.append(temp) # append to main list
+        print('temp all', temp_all)
+        return temp_all
+
+
+    # def is_leading_0(op_temp): # call function to check if leading 0s; if true, then call int_split
+
+    # def split_int()
+
+    # def compute_val()
+    # want only unique
+    p_ls = list(set(p_ls))
+    #print(p_ls)
     for p_tuple in p_ls:
         op_temp = []
         for k, p in enumerate(p_tuple):
             if (p==0):
-                op_temp.append((0))
+                op_temp.append([0])
             elif (k == 0) and (p==-1):
-                op_temp.append((2))
+                op_temp.append([2])
             else:
-                op_temp.append((i for i in operations))
+                op_temp.append([i for i in operations])
+        #print(op_temp)
+        op_temp_all_f.append(temp_format(op_temp)) # append the formatted list of op lists
+        #print(temp_format(op_temp))
         
 
-
-
-# for i, p in enumerate(p_perm):
-#     operations_temp= list(permutations(operations, i+1)) # list to hold the operations tuples. permute in groups of size = number of operations
-#     operations_perm.append(operations_temp)
+# call get_op_perms for all inputs and all perms-----------------
+# for inp in input_perm:
+for i, p in enumerate(p_perm):
+    get_op_perms(p)
 
 # print(operations_perm[-1])
 
