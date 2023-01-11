0b910954c0ebd60ce3fbd90d8711926786fd4e7d
oscars47 <scholinoscar@gmail.com>
2023-01-10 22:07:38 -0800

    need to fix build_eq

--- 2023/yearly.py
+++ 2023/yearly.py
@@ -13,6 +13,9 @@ from more_itertools import locate # get all locations of particular value in lis
 from sympy import solve, simplify # for solving string equations; for dealing with complex numbers
 from sympy.abc import x
 from sympy.parsing.sympy_parser import parse_expr
+from joblib import Parallel, delayed # for paralellization
+import multiprocessing
+from tqdm import tqdm # to give progress bars
 
 # define list of operations
 # 1 is +
@@ -50,9 +53,6 @@ p_3 = [p0, p1, p2, p3]
 # perform position permuations
 p_perm = [list(permutations(p)) for p in p_3]
 p_perm.append([tuple(p4)])
-# print(p_perm)
-# print(len(p_perm))
-# print(len(p_perm[1]))
 
 # now figure out possible operations, the integers to be used in the calcultations, and the final answer
 # for the first two, we use list of list of tuples; for the value, just list of lists of integer
@@ -60,43 +60,60 @@ op_perm_all = []
 int_order_all = []
 int_val_all = []
 
-#print(p_perm[0])
-
-
 
 # function to build  equation in strings
 def build_eq(int_split_temp, op_temp):
+    # print(int_split_temp)
+    # print(op_temp)
     eqn = ""
     split_index = 0
     a = 0
     while a < len(op_temp):
         op = op_temp[a]
+        # print('a', a)
+        # print('op', op)
+        
         if op==0: # if hit 0, keep going until no more 0s
+            # print('hjer')
             for b in range(a+1, len(op_temp), 1):
-                if (op_temp[b] != 0) or (b==len(op_temp)-1):
-                    eqn+=str(int_split_temp[split_index])
-                    split_index+=1
-                    a+=(b-a)-1
+                # print(split_index)
+                if (op_temp[b] != 0) or ((op_temp[b] == 0) and (b==len(op_temp)-1)):
+                    if split_index < len(int_split_temp):
+                        eqn+=str(int_split_temp[split_index])
+                        split_index+=1
+                    a+=(b-(a+1))
+                    break
+                    # print('a here', a)
         else:
             eqn+=str(op_dict[str(op)])
             if a == len(op_temp)-1: # if at the end, add the opreation and then the digit
-                eqn+=str(int_split_temp[split_index])
+                if split_index < len(int_split_temp):
+                    eqn+=str(int_split_temp[split_index])
                 break
-            elif op_temp[a+1]==0:
+            
+            if op != -3 and op_temp[a+1]!=0:
+                # if (op == -2) or (op_temp[a+1] == -2): # if we have open paren (
+                if op ==-2:
+                    eqn+=str(op_dict[str(op_temp[a+1])])
+                    # a+=1
+                    
+                if split_index < len(int_split_temp):
+                    eqn+=str(int_split_temp[split_index])
+                    split_index+=1
+                a+=1
+            else:
+                # print('here')
                 # if next one is 0, keep going until we run out of the 0s
                 for b in range(a+1, len(op_temp), 1):
-                    if (op_temp[b] !=0) or (b==len(op_temp)-1):
-                        eqn+=str(int_split_temp[split_index])
-                        split_index+=1
-                        a+=(b-a)-1
-            elif op != -3:
-                if (op == -2) or (op_temp[a+1] == -2): # if we have open paren (
-                    eqn+=str(op_dict[str(op_temp[a+1])])
-                    a+=1
-                    
-                eqn+=str(int_split_temp[split_index])
-                split_index+=1
+                    if (op_temp[b] !=0) or ((op_temp[b] == 0) and (b==len(op_temp)-1)):
+                        # print('split', split_index)
+                        if split_index < len(int_split_temp):
+                            eqn+=str(int_split_temp[split_index])
+                            split_index+=1
+                        a+=(b-(a+1))
+                        break
         a+=1
+            
     return eqn
  # note to actually solve this need to add 'x=' to beginning and call the compute function
 
@@ -106,12 +123,14 @@ def update(p_ls, inp): # takes in current p list and input numbers
     # op_total = [] # finalized list of all formatted op_temps
     # int_order_temp = [] # list to hold integers broken up by operations
     # int_val_temp = [] # list to hold computations for the integers and the operations
-    op_temp_f_all = [] # list to hold all formatted op temps, including parenthese
+    # op_temp_f_all = [] # list to hold all formatted op temps, including parenthese
     def temp_format(op_temp): # nested function to format the op_temps from [(2), (1, 2, 3, 4, 5), (0)] => [(2, 1, 0), (2, 2, 0), etc]
-        temp_all = []
+        
         # get count of full operations lists; this determines the r value for permutations
         r = op_temp.count(operations)
+        # print(r)
         if r > 0:
+            temp_all = []
             # add restrictive permutations as well
             choice_perm = []
             for i in range(len(operations)):
@@ -153,7 +172,12 @@ def update(p_ls, inp): # takes in current p list and input numbers
                         temp.append(choice[c])
                         c+=1
                 temp_all.append(temp) # append to main list
-        #print('temp all', temp_all)
+        else:
+            print(op_temp)
+            temporary = [op[0] for op in op_temp]
+            temp_all = []
+            temp_all.append(temporary)
+        # print('temp all', temp_all)
         return temp_all
 
     # helper function to compute string representation of split integers
@@ -202,14 +226,22 @@ def update(p_ls, inp): # takes in current p list and input numbers
 
     def is_not_leading_0(op_temp, inp): # call function to check if no leading 0s; if true, then call int_split
         int_split_str = parse_split_str(op_temp, inp)
+        #print(int_split_str)
         for int_split in int_split_str:
-            if (int_split[0]=='0') and (len(int_split[0]) > 1): # if first index is 0 of multicharacter integer, then return false: i.e. there IS a leading 0
+            #print(int_split[0])
+            if (int_split[0]=='0') and (len(int_split) > 1): # if first index is 0 of multicharacter integer, then return false: i.e. there IS a leading 0
+                #print('false')
                 return False
         return True # if we haven't returned False, it must be True
-        
+
+    # def not_divide_0(int_split_ls, op_temp):
+    #     for int_split in int_split_ls:
+    #         if int_split==0:
+
 
     def check_paren(op_temp): # function to return a list of parenthesis variations
         # initialize parentheses list
+        # print('op_temp', op_temp)
         op_paren = []
         # subfunction to check ok'd prior characters
         def get_paren_temp(op_index_ls, ok_ls):
@@ -275,43 +307,20 @@ def update(p_ls, inp): # takes in current p list and input numbers
             return ((int(int_val) - int_val == 0) and (int_val >= 1) and (int_val <= 100)) # conditions stated in problem
         else:
             return False # not real, so not integer
-
-    # want only unique
-    p_ls = list(set(p_ls))
-    #print(p_ls)
-    for p_tuple in p_ls:
-        op_temp = []
-        for k, p in enumerate(p_tuple):
-            if (p==0):
-                op_temp.append([0])
-            elif (k == 0) and (p==-1):
-                op_temp.append([2])
-            else:
-                op_temp.append([i for i in operations])
-        # get formatted op_temp
-        op_temp_f = temp_format(op_temp)
-        #print(op_temp_f)
-        
-        for op_temp in op_temp_f: # check each of these permutations and add paranetheses
-            if len(op_temp) > 0:
-                if is_not_leading_0(op_temp, inp):
-                    # check for parentheses: if vector has len > 0 then we can append
-                    #print(op_temp)
-                    paren_ls = check_paren(op_temp)
-                    #print(paren_ls)
-                    if len(paren_ls) >=1: # if have at least 1 paren variation, add it to main list
-                        #print(paren_ls)
-                        for paren_temp in paren_ls:
-                            op_temp_f.append(paren_temp)
-    
-        
-        for op_temp in op_temp_f: # go through and compute
+    def compute_all(op_temp):# go through and compute
+            # print(op_temp)
             if len(op_temp) > 0:
                 if is_not_leading_0(op_temp, inp): # if splitting doesn't result in leading 0s
+                    # print('not leading 0', op_temp)
                     int_split_temp = split_int(op_temp, inp) # get the split integers
                     int_val = compute_int_val(int_split_temp, op_temp) # compute the integer result
-                    #print(int_val)
+                    
+                    # display results in real time
+                    eqn = build_eq(int_split_temp, op_temp)
+                    print(str(int_val) + ' = ' + eqn)
+
                     if compute_pass(int_val): # if it passes then we can check it against existing results
+                        # print('compute pass', op_temp)
                         # see if it doesn't already exist; if so, then add all elements
                         if int_val_all.count(int_val) == 0:
                             int_val_all.append(int_val)
@@ -330,25 +339,90 @@ def update(p_ls, inp): # takes in current p list and input numbers
                                     op_perm_all[val_index] = op_temp
                                     int_order_all[val_index] = int_split_temp
                             # else if it's bigger, then don't do anything -- keep original
+                # else:
+                #     print('has leading 0s')
+    # want only unique
+    p_ls = list(set(p_ls))
+    #print(p_ls)
+    for p_tuple in p_ls:
+        # print(p_tuple)
+        op_temp = []
+        for k, p in enumerate(p_tuple):
+            if (p==0):
+                op_temp.append([0])
+            elif (k == 0) and (p==-1):
+                op_temp.append([2])
+            else:
+                op_temp.append([i for i in operations])
+        # get formatted op_temp
+        op_temp_f = temp_format(op_temp)
+        # print('before', op_temp_f)
+        # print(op_temp_f[0])
+        
+        paren_ls_ls = [] # list to store list of paren lists
+        for op_f in op_temp_f: # check each of these permutations and add paranetheses
+                # print(op_f)
+                # print(len(op_f))
+                if len(op_f) > 0:
+                    #print('here')
+                    if is_not_leading_0(op_f, inp):
+                        #print('getting paren')
+                        # check for parentheses: if vector has len > 0 then we can append
+                        #print(op_temp)
+                        paren_ls = check_paren(op_f)
+                        if len(paren_ls) > 0:
+                            paren_ls_ls.append(paren_ls)
+            
+
+        # now go through all paren_ls and append to op_temp_f
+        for paren_ls in paren_ls_ls:
+            for paren_temp in paren_ls:
+                op_temp_f.append(paren_temp)
+        # print('here')
+        #print(len(op_temp_f))
+        # since this is where we actually do all the computaytion, we will want to parallelize here
+        
+        # call parallelization
+        # num_cores = multiprocessing.cpu_count()
+        # Parallel(n_jobs=num_cores)(delayed(compute_all)(op_temp) for op_temp in op_temp_f)
+        
+        # non-parallelized
+        for op_temp in op_temp_f:
+            compute_all(op_temp)
+        # print('done with parallelization')
+
 # function to output the results
 def print_results():
+    def first(t):  
+        return t[0]    
+     
+    # function to sort the tuple     
+    def sort(tuple_ls):  
+        return sorted(tuple_ls, key = first)  
+    
+    # take all 3 lists, zip them together, then sort
+    results = list(zip(int_val_all, int_order_all, op_perm_all))
+    results_sorted = sort(results)
+    
+    
     print('Found %i solutions:' %len(int_val_all))
-    for i in range(len(int_val_all)):
-        int_val = int_val_all[i]
-        int_split = int_order_all[i]
-        op_temp = op_perm_all[i]
+    for result in results_sorted:
+        int_val = result[0]
+        int_split = result[1]
+        op_temp = result[2]
         eqn = build_eq(int_split, op_temp)
         print(str(int_val) + ' = ' + eqn)
 
-
 # call the main driver function-----------------
 def run():
-    for inp in input_perm:
+    for inp in tqdm(input_perm, desc='progress on input...', position=0):
     #inp = input_perm[0]
         for p_ls in p_perm:
-            update(p_ls, inp)
+            # update(p_ls, inp)
+            print('updated')
 
     # now call printing function for each value in the op_perm_all, int_order_all, and int_val_all
+    print('-----------------------')
     print_results()
 
 
